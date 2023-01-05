c8c5e8f5baa9827f0de55c7322de3ad4d53e4752
Aleksandra Wierzbicka <a.wierzbicka0579@gmail.com>
2023-01-05 19:14:24 +0100

    init commit

---
+++ src/main/java/pl/bubblenow/BubbleNowApplication.java
@@ -0,0 +1,13 @@
+package pl.bubblenow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BubbleNowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BubbleNowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/bubblenow/BubbleNowApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bubblenow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BubbleNowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
