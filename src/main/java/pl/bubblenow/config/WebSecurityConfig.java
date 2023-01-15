476ea2a653ab8c3de403dcfc878d7092787b527b
Aleksandra Wierzbicka <a.wierzbicka0579@gmail.com>
2023-01-15 15:44:35 +0100

    Changes: - fixed view for order without bubbleTea

--- src/main/java/pl/bubblenow/config/WebSecurityConfig.java
+++ src/main/java/pl/bubblenow/config/WebSecurityConfig.java
@@ -3,8 +3,10 @@ package pl.bubblenow.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -25,7 +27,7 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers("/").permitAll();
                     auth.requestMatchers("/css/**").permitAll();
